aeefddfccbd6ff5adeab9e503b7edee6cd6f5070
nabid97 <nabid1997@gmail.com>
2025-01-14 21:22:56 +0100

    Merge remote-tracking branch 'origin/main'

--- backend/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    roots: ["<rootDir>/backend"],
-    testEnvironment: "node",
-    moduleFileExtensions: ["js", "jsx", "json", "node"],
-    transform: {
-      "^.+\\.jsx?$": "babel-jest"
-    }
-  };
