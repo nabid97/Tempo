bcbcb14f140bde369f6c73469aa88b18d1d2e7c2
nabid97 <nabid1997@gmail.com>
2025-01-16 16:54:12 +0100

    Test 9

--- terraform/frontend/frontend-s3.tf
+++ terraform/frontend/frontend-s3.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "frontend" {
   bucket = "tempo-frontend-app-bucket-030125" # Replace with your unique bucket name
+  force_destroy = true
 
   tags = {
     Environment = "Production"
@@ -19,6 +20,14 @@ resource "aws_s3_bucket_website_configuration" "frontend_website" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+  
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
 resource "aws_s3_bucket_policy" "frontend_policy" {
   bucket = aws_s3_bucket.frontend.id
 
@@ -31,29 +40,34 @@ resource "aws_s3_bucket_policy" "frontend_policy" {
         Principal = "*",
         Action    = "s3:GetObject",
         Resource  = "${aws_s3_bucket.frontend.arn}/*"
+      },
+      {
+        Sid       = "AllowPutBucketPolicy",
+        Effect    = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::495599735948:user/NabidDev"
+        },
+        Action    = "s3:PutBucketPolicy",
+        Resource  = "${aws_s3_bucket.frontend.arn}"
       }
     ]
   })
 }
 
-resource "aws_s3_bucket_public_access_block" "frontend" {
-  bucket                 = aws_s3_bucket.frontend.id
-  block_public_acls      = false
-  ignore_public_acls     = false
-  block_public_policy    = false
-  restrict_public_buckets = false
+locals {
+  frontend_dist_path = "C:/Users/Nabid/Tempo/frontend/dist"
 }
 
 resource "aws_s3_object" "frontend_index" {
   bucket        = aws_s3_bucket.frontend.id
   key           = "index.html"
-  source        = "./dist/index.html"
+  source        = "${local.frontend_dist_path}/index.html"
   content_type  = "text/html"
 }
 
 resource "aws_s3_object" "frontend_js" {
   bucket       = aws_s3_bucket.frontend.id
   key          = "bundle.js"
-  source       = "./dist/bundle.js"
+  source       = "${local.frontend_dist_path}/bundle.js"
   content_type = "application/javascript"
 }
